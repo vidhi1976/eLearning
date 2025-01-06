0670be22cb3929c2e73b6db9a3f9096b55870059
Vidhi Singh <vidhisingh1956@gmail.com>
2025-01-07 00:36:53 +0530

    ui setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+  return (
+    <Button>lets build an lms</Button>
+      
+  )
+}
+
+export default App
