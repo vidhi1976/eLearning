a52ffe7059b7aa269b2214fbcce3c753b28675cb
Vidhi Singh <vidhisingh1956@gmail.com>
2025-01-30 16:36:35 +0530

    Navbar completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,65 +1,119 @@
-import { BookOpenCheck } from 'lucide-react'
-import React from 'react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
-import { Button } from './ui/button';
+import { BookOpenCheck, Menu } from "lucide-react";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import DarkMode from "@/DarkMode";
+import { Separator } from "@radix-ui/react-dropdown-menu";
 
 const Navbar = () => {
   const user = true;
+  
   return (
-    // desktop:
-    <div className='h-14 dark:bg-[#020817] bg-gray-300  fixed top-0 left-0 right-0 duration-300 z-10'>
-        <div className='max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full'>
-          <div className='flex items-center gap-2'>
-            <BookOpenCheck size={30}/>
-            <h1 className="hidden md:block font-extrabold text-2xl">
-                  eLearning
-                </h1>
-          </div>
-          {/* user icon and dark mode */}
-          <div>
-            {
-              user?(
-                <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline">Open</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            Profile
-            
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Billing
-            
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Settings
-            
-          </DropdownMenuItem>
-          
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-       
-        <DropdownMenuItem>
-          Log out
-        
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-              ):(
-                <div className='flex items-center gap-2'>
-                  <Button variant="outline">Login</Button>
-                  <Button>Signup</Button>
-                </div>
-              )
-            }
-          </div>
+    <div className="h-14 dark:bg-[#020817] bg-gray-300  fixed top-0 left-0 right-0 duration-300 z-10">
+      {/* // desktop: */}
+      <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
+        <div className="flex items-center gap-2">
+          <BookOpenCheck size={30} />
+          <h1 className="hidden md:block font-extrabold text-2xl">eLearning</h1>
         </div>
-    </div>
-  )
-}
+        {/* user icon and dark mode */}
+        <div className="flex items-center gap-8">
+          {user ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar>
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>My Learning</DropdownMenuItem>
+                  <DropdownMenuItem>Edit Profile</DropdownMenuItem>
 
-export default Navbar
+                  <DropdownMenuItem>Log out</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Dashboard</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <div className="flex items-center gap-2">
+              <Button variant="outline">Login</Button>
+              <Button>Signup</Button>
+            </div>
+          )}
+          <DarkMode />
+        </div>
+      </div>
+      {/* // mobile device */}
+      <div className="flex md:hidden items-center justify-between px-4 h-full">
+        <h1 className="font-extrabold text-2xl">eLearning</h1>
+        <MobileNavbar />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+const MobileNavbar = () => {
+  const role = "instructor";
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+          size="icon"
+          className="rounded-full bg-gray-100 text-black hover:bg-gray-200"
+          variant="outline"
+        >
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="flex flex-col ">
+        <SheetHeader className="flex flex-row items-center justify-between mt-2">
+          <SheetTitle>eLearning</SheetTitle>
+
+          <DarkMode />
+        </SheetHeader>
+        <Separator className="mr-2" />
+        <nav className="flex flex-col space-y-4">
+          <span>My Learning</span>
+          <span>Edit Profile</span>
+          <span>Log Out</span>
+        </nav>
+        {role === "instructor" && (
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit">Dashboard</Button>
+            </SheetClose>
+          </SheetFooter>
+        )}
+      </SheetContent>
+    </Sheet>
+  );
+};
